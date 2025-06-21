1c9d57fada67ae10e852a93c98cb876c1a3a1cf5
guptak-hash <guptakishan1123@gmail.com>
2025-06-21 21:19:13 +0530

    hash password

--- blog-backend/routes/user.routes.js
+++ blog-backend/routes/user.routes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const UserModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const UserRouter = express.Router();
 
 UserRouter.post('/signup', async (req, res) => {
     try {
-        // const user = req.body;
+        const {email,password} = req.body;
         // console.log(user);
-        const user=await UserModel.create(req.body);
+         const hash = await bcrypt.hash(password, saltRounds);
+        const user=await UserModel.create({email,password:hash});
         res.status(200).json({ msg: 'Signup success', user })
     }catch(err){
         console.log(err.message);
